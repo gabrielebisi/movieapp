5aad8b2be509e640fe5581c170b4476cc0390ed3
gabrielebisi <contact.gabrielebisi@gmail.com>
2022-05-25 11:38:15 +0200

    aggiunto index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const URL_TITANIC = "http://www.omdbapi.com/?apikey=78c925e7&s=titanic";
+
+export const titanic = () => {
+    fetch(URL_TITANIC)
+    .then(jsonResponse => jsonResponse.json())
+    .then(objResult => {
+        const movie = objResult.Search;
+        console.log(movie);
+    });
+}
